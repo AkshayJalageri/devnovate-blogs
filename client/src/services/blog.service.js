6052b42eb30848fbfdc70ef9e8bdd5a1c7ca6c45
Akshay Jalageri <167541593+AkshayJalageri@users.noreply.github.com>
2025-08-31 01:45:11 +0530

    Update blog.service.js

--- client/src/services/blog.service.js
+++ client/src/services/blog.service.js
@@ -1,13 +1,12 @@
 import api from './api';
 
 const BlogService = {
-  // Get all blogs with optional pagination and filters
   getAllBlogs: async (page = 1, limit = 10, search = '', tags = []) => {
     try {
       let query = `/blogs?page=${page}&limit=${limit}`;
       if (search) query += `&search=${search}`;
-      if (tags && tags.length > 0) query += `&tags=${tags.join(',')}`;
-      
+      if (tags.length > 0) query += `&tags=${tags.join(',')}`;
+
       const response = await api.get(query);
       return response.data;
     } catch (error) {
@@ -15,7 +14,6 @@ const BlogService = {
     }
   },
 
-  // Get trending blogs
   getTrendingBlogs: async (limit = 5) => {
     try {
       const response = await api.get(`/blogs/trending?limit=${limit}`);
@@ -25,7 +23,6 @@ const BlogService = {
     }
   },
 
-  // Get a single blog by ID
   getBlogById: async (id) => {
     try {
       const response = await api.get(`/blogs/${id}`);
@@ -35,7 +32,6 @@ const BlogService = {
     }
   },
 
-  // Create a new blog
   createBlog: async (blogData) => {
     try {
       const response = await api.post('/blogs', blogData);
@@ -45,7 +41,6 @@ const BlogService = {
     }
   },
 
-  // Update a blog
   updateBlog: async (id, blogData) => {
     try {
       const response = await api.put(`/blogs/${id}`, blogData);
@@ -55,7 +50,6 @@ const BlogService = {
     }
   },
 
-  // Delete a blog
   deleteBlog: async (id) => {
     try {
       const response = await api.delete(`/blogs/${id}`);
@@ -65,7 +59,6 @@ const BlogService = {
     }
   },
 
-  // Like a blog
   likeBlog: async (id) => {
     try {
       const response = await api.post(`/blogs/${id}/like`);
@@ -75,7 +68,6 @@ const BlogService = {
     }
   },
 
-  // Add a comment to a blog
   addComment: async (id, content) => {
     try {
       const response = await api.post(`/blogs/${id}/comments`, { content });
@@ -85,7 +77,6 @@ const BlogService = {
     }
   },
 
-  // Get comments for a blog
   getComments: async (id, page = 1, limit = 10) => {
     try {
       const response = await api.get(`/blogs/${id}/comments?page=${page}&limit=${limit}`);
@@ -95,7 +86,6 @@ const BlogService = {
     }
   },
 
-  // Get user's blogs
   getUserBlogs: async () => {
     try {
       const response = await api.get('/blogs/user');
@@ -105,7 +95,6 @@ const BlogService = {
     }
   },
 
-  // Get user's liked blogs
   getLikedBlogs: async () => {
     try {
       const response = await api.get('/blogs/liked');
@@ -115,7 +104,6 @@ const BlogService = {
     }
   },
 
-  // Get blogs by a specific user (public profile)
   getBlogsByUser: async (userId, page = 1, limit = 10) => {
     try {
       const response = await api.get(`/users/${userId}/blogs?page=${page}&limit=${limit}`);
@@ -123,7 +111,7 @@ const BlogService = {
     } catch (error) {
       throw error.response?.data || { message: 'An error occurred while fetching user blogs' };
     }
-  },
+  }
 };
 
 export default BlogService;
